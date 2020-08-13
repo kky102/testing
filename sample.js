9cffbeeebd0df2c189ea7de87821077fa118f2b4
kykim102 <58721219+kykim102@users.noreply.github.com>
2020-08-13 20:49:16 +1200

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,39 +1,5 @@
-import React from 'react'
-import Head from 'next/head'
-import Link from 'next/link'
-
-export default () => {
-    const intensity = 'camping'
-
-    return (
-        <div className="root">
-            <Head>
-                <meta charSet="utf-8"/>
-                <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-                <meta name = "viewport" content="width=device-width, initial-scale=1"/>
-                <title>Wild Lamb testing</title>
-            </Head>
-            <style jsx>{`
-            .root {
-                font-familyL sans-serif;
-                line-height: 1.33rem;
-                margin-top: 8vh,
-            }
-            @media (min-width: 600px) {
-                .root {
-                    margin-left: 21vw;
-                    margin-right: 21vw;
-                }
-            }
-            img {
-                max-width: 100%;
-            }
-            `}</style>
-
-            <h1>Please work</h1>
-            <h2>{`Book your ${intensity.toUpperCase()} Trip`}</h2>
-            <p><Link href="/trips"><a>Sign-UP Now</a></Link></p>
-            <img src={`/testing/${intensity}-camping.jpg`} alt={intensity}/>
-        </div>
-    )
-}
+var http = require('http');
+http.createServer(function(req,res){
+  res.writeHead(200,{'Content-Type': 'Text/html'});
+  res.end('Hello World');
+}).listen(8080);
